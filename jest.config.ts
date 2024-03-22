0005b4d998593580893ff3d1989519b28113b3c4
Rodrigo Godoi <rhog.dev@gmail.com>
2024-03-21 23:19:37 -0300

    infra/db - completed

--- jest.config.ts
+++ jest.config.ts
@@ -175,17 +175,15 @@ const config: Config = {
   // testRunner: "jest-circus/runner",
 
   // A map from regular expressions to paths to transformers
-  // transform: undefined,
+  transform: {
+    '^.+\\.(t|j)sx?$': '@swc/jest',
+  },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   // transformIgnorePatterns: [
   //   "/node_modules/",
   //   "\\.pnp\\.[^\\/]+$"
   // ],
-
-  transform: {
-    '^.+\\.(t|j)s$': '@swc/jest',
-  },
 
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
   // unmockedModulePathPatterns: undefined,
